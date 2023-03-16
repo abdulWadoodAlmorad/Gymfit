59fedf050d995119f6f29858388bda2502ed1c5e
abdulWadoodAlmorad <abdoalmorad1995@gmail.com>
2023-03-16 11:15:35 +0100

    Upload

---
+++ gym.js
@@ -0,0 +1,19 @@
+/*Start Scroll */
+
+let span = document.querySelector(".up");
+
+
+window.onscroll= function(){
+    this.scrollY >= 1000 ? span.classList.add("show") : span.classList.remove("show");
+
+};
+span.onclick= function(){
+    window.scrollTo({
+        top :0,
+        behavior:"smooth",
+    });
+};
+/*End Scroll */
+
+
+
